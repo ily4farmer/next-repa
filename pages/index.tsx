0d40a5d24e5d81c5a1aadd636b5fb50e34b8d044
Lexa <ily4.farmer@yandex.ru>
2022-11-10 02:33:59 +0300

    add prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,29 @@ import Layout from '../components/Layout/Layout'
 
 export default function Home() {
   const [name, setName] = useState<string>('')
-  const [list, setList] = useState<any>([])
+  const [list, setList] = useState<number[]>([])
 
-  const send = async() => {
-    await axios.post('http://localhost:5700/api/user/', {name: name})
+  const send = async () => {
+    await axios.post('http://localhost:5700/api/user/', { name: name })
     const res = await (await axios.get('http://localhost:5700/api/user/')).data
     setList(res)
-    console.log(res);
+    console.log(res)
   }
 
   return (
     <Layout>
       <div className="container">
         <div className="bg-text rounded-lg text-base text-center">
-          <input value={name} onChange={(e) => setName(e.target.value)} type="text" />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+          />
           <button onClick={send}>Send request</button>
           <ul>
-            {list.map((el: any) => <li key={el.id}>{el.name}</li>)}
+            {list.map((el: any) => (
+              <li key={el.id}>{el.name}</li>
+            ))}
           </ul>
         </div>
       </div>
